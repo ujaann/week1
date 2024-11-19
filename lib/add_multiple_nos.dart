d7e3e569ee14569202c559c477f790f356eb8420
ujaann <ujanst@gmail.com>
2024-11-19 10:33:47 +0545

    Function to return simple interest

--- lib/add_multiple_nos.dart
+++ lib/add_multiple_nos.dart
@@ -3,6 +3,8 @@ void main() {
 }
 
 double simpleInterest(
-    {required double principle, required double rate, required double time}) {
+    {required double principle,
+     required double rate,
+      required double time}) {
   return (principle * rate * time) / 100;
 }
