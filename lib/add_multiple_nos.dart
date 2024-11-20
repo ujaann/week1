858142f9d032c65888d4760ae5b0ff3169c6ce61
ujaann <ujanst@gmail.com>
2024-11-20 09:00:27 +0545

    Added string interpolation to print output

--- lib/add_multiple_nos.dart
+++ lib/add_multiple_nos.dart
@@ -1,5 +1,9 @@
 void main() {
-  print(simpleInterest(principle: 1000.12, rate: 12, time: 2));
+  double principle=112300;
+  double time=2;
+  double rate=10;
+
+  print("The simple interest on principle: $principle with rate $rate % for $time years is:\n${simpleInterest(principle: principle, rate: rate, time: time)}");
 }
 
 double simpleInterest(
