f913ed69923df91c4c976cbe5f596ec89be28560
ujaann <ujanst@gmail.com>
2024-11-20 09:04:12 +0545

    Added null coalescing operator so that when rate is not provided a default value is used

--- lib/add_multiple_nos.dart
+++ lib/add_multiple_nos.dart
@@ -3,12 +3,12 @@ void main() {
   double time=2;
   double rate=10;
 
-  print("The simple interest on principle: $principle with rate $rate % for $time years is:\n${simpleInterest(principle: principle, rate: rate, time: time)}");
+  print("The simple interest on principle: $principle with rate $rate % for $time years is:\n${simpleInterest(principle: principle, time: time,rate: rate)}");
 }
 
 double simpleInterest(
     {required double principle,
-     required double rate,
+      double? rate,
       required double time}) {
-  return (principle * rate * time) / 100;
+  return (principle * (rate??6.8) * time) / 100;
 }
